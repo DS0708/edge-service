ea87091a5356f4d7417c4403417c917bd5e57c95
DS0708 <jonum12312@gmail.com>
2024-10-17 14:02:39 +0900

    chapter11-issue1-test

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.polarbookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -8,12 +9,16 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import org.springframework.boot.autoconfigure.security.oauth2.client.reactive.ReactiveOAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.reactive.ReactiveOAuth2ResourceServerAutoConfiguration;
 
 //일단, 레디스가 웹 세션 관련 데이터를 저장 시 Spring Context가 올바르게 로드되는지 확인
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)   //완전한 Spring Web Application Context와 Random port를 사용중인 웹 환경을 로드
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration(exclude = { //OAuth2/Keycloak 관련 의존성 없이도 컨텍스트가 정상적으로 로드되게 함
+        ReactiveOAuth2ClientAutoConfiguration.class,
+        ReactiveOAuth2ResourceServerAutoConfiguration.class
+})  //완전한 Spring Web Application Context와 Random port를 사용중인 웹 환경을 로드
 @Testcontainers //테스트 컨테이너의 자동 시작과 종료를 활성화
 class EdgeServiceApplicationTests {
 
